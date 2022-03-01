fd5b9d2a7040fc8c54bab092e4f34af44cf9ff76
Denys Bahachuk <bagachukdg@gmail.com>
2022-03-01 14:14:03 +0200

    Create changeString.go
    A program that takes three arguments: the name of a text file, and two
    strings. This utility replaces every occurrence of the first string in the
    file with the second string. For reasons of security, the final output will be
    printed on the screen, which means that the original text file will remain intact.

---
+++ changeString.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	file1 := flag.String("f", "", "filePath")
+	str1 := flag.String("s1", "", "string1")
+	str2 := flag.String("s2", "", "string2")
+	flag.Parse()
+
+	fmt.Println(flag.NFlag())
+	if flag.NFlag() < 3 {
+		fmt.Println("Not enough arguments!")
+		os.Exit(1)
+	}
+
+	fileName := *file1
+	string1 := *str1
+	string2 := *str2
+
+	file, err := os.Open(string(fileName))
+	defer file.Close()
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		os.Exit(2)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.TrimRight(line, "\r\n")
+		if line == string1 {
+			line = string2
+		}
+		fmt.Println(line)
+	}
+}
